ed5aced03f92a39e39f4fd61d6d405d0f0d5eb38
Abdulkadir Aba <abdulkadir.aba786@gmail.com>
2020-05-14 12:58:41 -0700

    aalayouts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Secound from './layout/Secound'
+import {  BrowserRouter as Router, Route } from "react-router-dom";
+// import './App.css';
+import logIn from './layout/Login';
+import Navbar from './components/Navbar';
+import Patient from './layout/Patient';
+import API from './utils/API';
+import Footer from "./components/Footer";
+import LogBook from './layout/LogBook'
+import Login from './layout/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = { data: [], user: {} }
+
+  componentDidMount() {
+    API.getAllPatients().then(({ data }) => {
+      this.setState({ data })
+      console.log(data)
+    })
+  }
+  
+
+
+  render() {
+    return (
+      <Router>
+          <Navbar />
+          <Route exact path={["/","/login"]} component={Login} />
+          <Route exact path="/logbook" component={LogBook} />
+          <Route exact path="/patient/:id" component={LogBook} />
+          {/* <Route exact path="/patient:id" component={Patient} /> */}
+          <Route exact path="/patient" component={() => <Patient data={this.state.data} />} />
+          {/* <Route exact path="/patient" component={Secound}/> */}
+          <Footer />
+      </Router>
+    );
+  }
+
 }
+
+
 
 export default App;
---
+++ src/utils/API.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import logInbtn from "../components/LogInBtn"; 
+const BASE_URL = "http://localhost:8080"
+
+
+export default {
+getAllPatients: ()=> axios.get(`${BASE_URL}/patients`),
+getPatientLog: (id)=> axios.get(`${BASE_URL}/patient/${id}`),
+
+ // login btn
+ getLogInbtn: (id) =>
+   axios.post(`${BASE_URL}/` + id),
+
+// creating a route that appdates the patients ifo
+editPatient:(data,id)=>axios.get(`${BASE_URL}/patient/${id}`, data),
+login: (data)=> axios.post(`${BASE_URL}/api/login`, data),
+signup: (data)=> axios.post(`${BASE_URL}/api/signup`, data),
+
+  }
