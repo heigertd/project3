d2a33e7515c297f1ba290ada927daf6791c33587
yaya <anbesie.yg@gmail.com>
2020-05-19 16:07:50 -0700

    some style file edited

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../layout/style.css";
+// import "../layout/style.css";
 import { Link, useHistory } from "react-router-dom";
 //
 // import LogInBtn from '../LogInBtn';
