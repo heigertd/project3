0a6560cee9455b2eef87dedbf317c8d068b40e7f
Devin Heigert <heigertd@gmail.com>
2020-05-20 15:19:30 -0700

    local storage fix, clean up code

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,123 +1,74 @@
-
-
-
-
-  import React from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
 
 export default (props) => {
-  console.log("props.currentUser",props.currentUser.user)
+  console.log(props)
   return(
-  <>
     <nav>
-      <div class="nav-wrapper">
-
-        <a href="#" className="brand-logo center">DAYA App</a>
-
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-          {props.currentUser && props.currentUser.user? <h3>Welcome, {props.currentUser.user.username}</h3>:" login"}
-         
-          </li>
-          <li className="nav-item">
-          {props.currentUser? <Link
-              to="/patient"
-              className={window.location.pathname === "/patient" ? "nav-link active" : "nav-link"}
-            >
-              Patient
-            </Link>:""}
-          </li>
-          <li className="nav-item">
-              <Link
-              to="/patients"
-              className="nav-link"
-            >
-            Logbook
-            </Link>:""}
-          </li>
-      {props.currentUser &&props.currentUser.user && props.currentUser.user.isAdmin ?
-      
-        <>
-          <li className="nav-item">
-          <Link
-              to="/patients"
-              className="nav-link"
-            >
-             Manager
-            </Link>
-          </li>
-          
-
-          <li className="nav-item">
-          <Link
-              to="/employee"
-        
-
-            >
-             AddEmployee
-            </Link>
-          </li>
-          <li className="nav-item">
-          <Link
-              to="/newEntry"
-      
-            >
-             Addpatient
-            </Link>:""}
-          </li>
-          <li className="nav-item">
-          {props.currentUser &&props.currentUser.user? <Link
-              to="/manager"
-              className={window.location.pathname === "/manager" ? "nav-link active" : "nav-link"}
-
-            >
-            AddManager
-            </Link>:""}
-          </li>
-          <Link
-              to="/"
-              className="nav-link">
-              Log Out
-            </Link>
-          </> : ""}
-
-          {props.currentUser &&props.currentUser.user ? <li className="nav-item">
-          <Link
-              to="/"
-              className="nav-link">
-              Log Out
-            </Link>
-          </li> : <>
-          <li className="nav-item">
-          <Link
-              to="/"
-              className="nav-link">
-              Login 
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="signup"
-             className="nav-link"
-            >
-          Sign Up
-            </Link>
-          </li>
-          </>}
-          
-
-        </ul>
-
-      </div>
-      </div>
-    </nav>
-  </>
-  )}
-
-return(
-  <nav>
+    <div className='nav-wrapper'>
+      <a href="#" className="brand-logo center">DAYA App</a>
     
+    <ul className='navbar-nav'>
+      <li className='nav-item'>
+        {props.currentUser ? <h3>Welcome, {props.currentUser.username}</h3> : ""}
+      </li>
+      {props.currentUser ?
+        <div>
+          <li className='nav-item'>
+            <Link to="/patient" className={window.location.pathname === "/patient" ? "nav-link active" : "nav-link"}>
+              Patient
+              </Link>:
+          </li>
+          <li className='nav-item'>
+            <Link to="/patients" className="nav-link">
+              Logbook
+              </Link>
+          </li>
+        </div> : ''}
+      {props.currentUser.user && props.currentUser.user.isAdmin ?
+        <div>
+          <div>
+            <li className="nav-item">
+              <Link to="/patients" className="nav-link">
+                Manager
+                </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/employee">
+                AddEmployee
+                </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/newEntry">
+                Addpatient
+                </Link>
+            </li>
+          </div>
+          {props.currentUser.user ?
+            <div>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Log Out
+                  </Link>
+              </li>
+            </div>
+            : 
+            <div>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="signup" className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>}
+        </div>
+      : ''}
+    </ul>
+    </div>
   </nav>
-)
-          
+  )
+}
